5236e7b0d539ccc3b7d85508c54c5ca8c9646674
Muhammad Rabee Amir <rabeeamir25@gmail.com>
2024-07-03 11:01:18 +0500

    second commit

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    
+    return render(request, 'home.html', {})
+
+# def logout_user (request):
+#     pass
